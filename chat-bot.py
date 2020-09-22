0bd08498e8c8ce8210537f00818d64e89fd7648b
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2020-09-22 13:33:34 -0700

    update

--- chat-bot.py
+++ chat-bot.py
@@ -2,18 +2,12 @@ from random import choice
 
 def get_bot_response(user_response):
     #types of food response suggesstions
-    bot_response_indian = ["samosa", "tikka masala", "chana masala", "saag paneer", 
-        "vindaloo", "dal", "curry"]
-    bot_response_italian = ["spaghetti", "caprese salad", "ravioli", "gnocchi", 
-        "pizza", "charcuterie board", "fettuccine alfredo", "pasta primavera"]
-    bot_response_american = ["burger", "fries", "salad", "mac and cheese", 
-        "clam chowder", "bbq"]
-    bot_response_mexican = ["empenadas", "floutas", "tacos", "burrito", 
-        "chillaquiles", "chile relleno", "posole", "menudo"]
-    bot_response_japanese = ["sushi", "ramen", "tempura", "soba noodles",
-        "sashimi", "miso soup", "edamame", "mochi"]
-    bot_response_greek = ["gyro", "dolmas", "moussaka", "souvlaki", "tzatziki",
-        "baklava", "spanakopita", "hummus", "greek salad"]
+    bot_response_indian = ["samosa", "tikka masala", "chana masala", "saag paneer", "vindaloo", "dal", "curry"]
+    bot_response_italian = ["spaghetti", "caprese salad", "ravioli", "gnocchi", "pizza", "charcuterie board", "fettuccine alfredo", "pasta primavera"]
+    bot_response_american = ["burger", "fries", "salad", "mac and cheese", "clam chowder", "bbq"]
+    bot_response_mexican = ["empenadas", "floutas", "tacos", "burrito", "chillaquiles", "chile relleno", "posole", "menudo"]
+    bot_response_japanese = ["sushi", "ramen", "tempura", "soba noodles", "sashimi", "miso soup", "edamame", "mochi"]
+    bot_response_greek = ["gyro", "dolmas", "moussaka", "souvlaki", "tzatziki", "baklava", "spanakopita", "hummus", "greek salad"]
 
     #if statements for user input on type of food
     if user_response == "indian":
@@ -22,6 +16,8 @@ def get_bot_response(user_response):
         return choice(bot_response_italian)
     elif user_response == "american":
         return choice(bot_response_american)
+    elif user_response == "mexican":
+        return choice(bot_response_mexican)
     elif user_response == "japanese":
         return choice(bot_response_japanese)
     elif user_response == "greek":
@@ -30,12 +26,12 @@ def get_bot_response(user_response):
         return "When in doubt, look at Yelp!"
 
 print("Welcome to Food Bot!")
-print("What type of food are you craving? I will give a suggestion on what to order... ")
+print("Craving something? I will give a suggestion on what to order... ")
 
 user_response = ""
 
 while True:
-  user_response = input("What kind of food are you craving? ")
+  user_response = input("What type of food are you craving? ")
   
   # Quits program when user responds with 'done'
   if user_response == "done":
